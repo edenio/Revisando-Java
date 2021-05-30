82c46c845b2c15aefcf4df59b889fd60fa825e12
Edenio Alves de Oliveira <edenio_cdi@yahoo.com.br>
2021-05-30 12:02:25 -0300

    DEBUG Execução passo a passo

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,23 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		double largura = sc.nextDouble();
+		double comprimento = sc.nextDouble();
+		double metroQuadrado = sc.nextDouble();
 
-		int hora;
+		double area = largura * comprimento;
+		double preco = area * metroQuadrado;
 
-		System.out.println("Quantas Horas?");
-		hora = sc.nextInt();
+		System.out.printf(" AREA = %.2f%n", area);
+		System.out.printf("PREÇO = %.2f%n", preco);
 
-		if (hora < 12) {
-			System.out.println("Bom dia");
-		} 
-		else if (hora < 18) {
-			System.out.println("Boa tarde");
-		}
-		else {
-			System.out.println("Boa Noite");
-		}
 		sc.close();
+
 	}
 }
