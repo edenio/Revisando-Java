6ba2da0adc4919eeb6eeadc3fe04450246065581
Edenio Alves de Oliveira <edenio_cdi@yahoo.com.br>
2021-05-30 12:41:46 -0300

    DEBUG Execução While (Estrutura repetitiva)

--- src/Main.java
+++ src/Main.java
@@ -7,17 +7,15 @@ public class Main {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		double largura = sc.nextDouble();
-		double comprimento = sc.nextDouble();
-		double metroQuadrado = sc.nextDouble();
-
-		double area = largura * comprimento;
-		double preco = area * metroQuadrado;
-
-		System.out.printf(" AREA = %.2f%n", area);
-		System.out.printf("PREÇO = %.2f%n", preco);
-
-		sc.close();
-
+		int x = sc.nextInt();
+		int soma = 0;
+		
+		while(x !=0) {
+			soma += x;
+			x = sc.nextInt();
+							
+		}
+		System.out.println(soma);
+	sc.close();
 	}
 }
