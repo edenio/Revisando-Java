897e80e3b2b15cce6448abb4c8bfee34d47851de
Edenio Alves de Oliveira <edenio_cdi@yahoo.com.br>
2021-05-30 14:08:51 -0300

    operadores bitwise

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,12 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-		
-		char resp;	
-		do {
-			System.out.print("Digite a temperatura em Celsius");
-			double C = sc.nextDouble();
-			double F = 9.0 * C / 5.0 + 32.0;
-			System.out.printf("Equivalente em Fahrenheit: %.1f%n", F);
-			System.out.println("Deseja repetir (S/N)? ");
-			resp = sc.next().charAt(0);
-					
-		} while (resp != 'N');
-		
-		sc.close();
-			
-		}
+		int n1 = 89;
+		int n2 = 60;
+		System.out.println(n1 & n2);
+		System.out.println(n1 | n2);
+		System.out.println(n1 ^ n2);
 						
 			
 	}
+}
