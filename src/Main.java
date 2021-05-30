216b8a1b58ae668b8c140538f4de43f1977c2f9a
Edenio Alves de Oliveira <edenio_cdi@yahoo.com.br>
2021-05-30 16:26:30 -0300

    Funções em Java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,42 @@
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		int n1 = 89;
-		int n2 = 60;
-		System.out.println(n1 & n2);
-		System.out.println(n1 | n2);
-		System.out.println(n1 ^ n2);
-						
-			
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter three numbers");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+
+		int higher = max(a, b, c);
+
+		showResult(higher);
+
+		sc.close();
+
+	}
+
+	public static int max(int x, int y, int z) {
+		int aux;
+		if (x > y && x > z) {
+			aux = y;
+
+		} else if (y > z) {
+			aux = y;
+
+		} else {
+			aux = z;
+		}
+		return aux;
+
+	}
+
+	public static void showResult(int value) {
+
+		System.out.println("Higher = " + value);
+
 	}
 }
